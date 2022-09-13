3e2dd427dbab73dad8cb5b40b3a25e930612533b
bdehapiot <b.dehapiot@gmail.com>
2022-09-13 11:07:02 +0200

    update

--- predict_model.py
+++ predict_model.py
@@ -8,11 +8,17 @@ from stardist.models import StarDist2D
 
 from functions import process_data, norm_data
 
+#%% Inputs
+
+radius = 3 # radius for rolling ball background subtration (0 = deactivate)
+
 #%% Get stack_name
 
 # stack_name = '20mbar_end_AV_20171019_162451_substack(1-900-5).tif'
 # stack_name = '35mbar_end_05_20170613_151622_substack(1-900-5).tif'
-stack_name = '50mbar_start_00_20170613_111653_substack(1-900-5).tif'
+# stack_name = '50mbar_start_00_20170613_111653_substack(1-900-5).tif'
+# stack_name = '22-09-09_ML30_outllet_donneurMPZ_Temp250_x20_DeltaP05mBars.tif'
+stack_name = '22-09-09_ML30_outllet_donneurMPZ_Temp250_x20_DeltaP20mBars_substack(1-800-5).tif'
 
 #%% Paths & import
 
@@ -21,13 +27,15 @@ model = StarDist2D(None, name='stardist', basedir='models')
 
 #%% Predict
 
-stack = process_data(stack, radius=3, parallel=True)
+t = 400
+
+stack = process_data(stack, radius=radius, parallel=True)
 stack = norm_data(stack, qlow=0.001, qhigh=0.999)
-labels, details = model.predict_instances(stack[0,...])
+labels, details = model.predict_instances(stack[t,...])
 
 #%% Display
 
 viewer = napari.Viewer()
-viewer.add_image(stack[0,...])
+viewer.add_image(stack[t,...])
 viewer.add_labels(labels)
 # viewer.grid.enabled = True
